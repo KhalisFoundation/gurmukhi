e5199fa2afa9e330c14623340c25cd5568746ca5
Gurmehar Singh <gurmeharsinghkhalsa1005@gmail.com>
2023-09-30 13:24:08 +1000

    fix: adds support for phrases

--- shabadkosh/src/components/util/index.ts
+++ shabadkosh/src/components/util/index.ts
@@ -1,3 +1,8 @@
 export * from './controller';
 export * from './users';
 export * from './utils';
+
+export const removeExtraSpaces = (str: string) => {
+  if (!str) return '';
+  return str.replace(/\s\s+/g, ' ').trim();
+};
